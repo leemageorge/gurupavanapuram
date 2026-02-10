6611acf4467ab35d1d242343c0d7e4610afb8442
leemageorge555 <leemageorge555@gmail.com>
2026-02-10 22:37:45 +0530

    package,json updated

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div className='bg-amber-100 min-h-screen w-full relative py-14'>
+    <div className='bg-amber-100 min-h-screen w-full relative '>
       <form className='max-w-md mx-auto p-6 bg-yellow-200 '>
           <h2>Contact Form</h2>
         
